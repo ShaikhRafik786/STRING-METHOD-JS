e9f3c283d7ac89e06a25deddf697d4c237719e43
Shaikh RAFIK <107038358+ShaikhRafik786@users.noreply.github.com>
2023-02-01 23:07:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,204 @@
+//  padStart: padStart() supports padding at the beginning of a string.
+
+// var str = "javascript";
+// var rk = str.padStart();
+// console.log(rk);
+// document.write(rk);
+
+// var str = "javascript";
+// var r=str.padEnd();
+// console.log(r)
+// document.write(r);
+
+// lentgh--->it returns total length of character in no present in ur string
+// var str="bilolee";
+// var a= str.length
+// console.log(a)
+// document.write(a);
+
+//uppercase() it converts string into caps
+// var str="musalmanbadi";
+// var b=str.toUpperCase();
+// console.log(b)
+// document.write(b);
+
+//toLowerCase() it converts string into small letters
+// var str="AHEMADPUR";
+// var c=str.toLowerCase();
+// console.log(c)
+// document.write(c);
+//toLowerCase() it converts string into small letters
+
+// var str="Returns true if searchString appears as a substring of the result of converting this object to a String, at one or more positions ";
+// var d=str.includes("true");
+// console.log(d)
+// // document.write(d);
+// console.log(d)
+
+// var str="javascript---startsWith()--->returns true or false if string starts or not start with given argument";
+// var e=str.startsWith("javascript");
+// console.log(e)
+
+// document.write(e);
+
+// var str="javascript---endsWith()--->returns true or false if string ends or not ends with given argument";
+// var e=str.endsWith("rk");
+// // // document.write(e);
+// console.log(e)
+
+// match()--->it returns the string if is in it else null
+// var str="rafiq isa good boy";
+// var f=str.match("is"); 
+// // document.write(f);
+console.log(f)
+
+
+// indexof --->it returns index no
+// var str="rafiq is a good boy     100";
+// var f=str.indexOf("0");
+// console.log(f);
+// document.write(f);
+
+// lastIndexOf---> it returns index else -1 nahi hoga to usme
+// var str="rafiq is a good boy     100";
+// var f=str.lastIndexOf("y");
+// console.log(f)
+// document.write(f);
+
+// replace---> it can replace a part of string with the given argument
+// var str=" sayyed sohel salim";
+// var g=str.replace("sohel","shoaib");
+// console.log(g)
+// document.write(g);
+
+
+
+// it remove un necessory space from the start and end of the string
+// var str="   javascript khan     ";
+// var h=str.trim();
+// console.log(h);
+// document.write(h);
+
+// var str="Returns the character at the specified index.";
+// var h=str.charAt(22);
+// console.log(h)
+// document.write(h);
+
+// var str="Returns the character at the specified index.";
+// var h=str.charCodeAt(5);
+// console.log(h)
+// document.write(h);
+
+// var str="This is--->it help to join";
+// var str1="go to hell ";
+// var str2="cool baby";
+// var h=str.concat(str1,str2);
+// console.log(h)
+// document.write(h);
+
+// var str="repeat-->string into no of times.";
+// var h=str.repeat(30);
+// document.write(h);
+
+//to convert in String
+// var str=15;
+// var h=str.toString();
+// console.log(typeof(5))
+
+// document.write(h);
+
+//it returns index if it exist in else return-1
+// var str="shahrukh khan is king of bollywood";
+// var j=str.search("of");
+// console.log(j)
+// document.write(j);
+
+// extracts a part of string & return a new string (starts index,end index)
+// var str="javascript";
+// var h=str.slice(0,4);
+// console.log(h)
+// document.write(h);
+
+// var str="javascriptis a programming language";
+// var h=str.slice(0,10);
+// console.log(h)
+// document.write(h);
+
+//similar to slice method the only difference it doesnt except -ve index value
+// var str="javascriptispowerfull";
+// var h=str.substring(12,17);
+// console.log(h)
+// document.write(h);
+
+//Padstart (support padding add the start of a string)
+// var str = "javascript";
+// var sk = str.padStart("12" ,"khan");
+// console.log(sk)
+// document.write(sk);
+// console.log(sk);
+// Padend (support padding add the end of the string)
+// var str = "javascript";
+// var rk = str.padStart();
+// // document.write(rk);
+// console.log(rk);
+
+// String Methods //
+
+// 1. Slice :extracts a part of string and returns a new string. It accepts two arguments, start index & end index (excludes end index)
+ 
+// 2. SubString: similar to slice method. The only difference is it doesn't accept negative index value.
+
+// 3. Substr. similar to slice method. It accepts two arguments, start index & number of characters to be extracted
+
+// 4. Replace: it can replace a part of string with the given argument.
+
+// 5. toUpperCase : converts the string into ALL CAPS
+
+// 6. toLowerCase: converts the string into a string with all small letters.
+
+// 7. concat: it helps join two strings
+
+// 8. padStart: padstart() supports padding at the beginning of a string
+
+// 9. padEnd: padEnd() supports padding at the end of a string
+
+// 10. chartAt: this methods returns the letter at the given index
+
+// 11. split: it splits the string into miltiple parts taking the argument into consideration. It returns an array of the splitted parts.
+
+// 12. length: it returns the number total characters in your string.
+
+// 13. StartsWith: returns true or false if the string starts or doesn't start with the given argument.
+
+// 14. endsWith: returns true or false if the string ends or doesn't end with the given argument.
+
+// 15.  trim: it removes unncessary spaces from the start and end of the string. 
+
+//16. trimRight: trims removes white spaces from the right side of the string.
+
+//17. trimLeft: trims removes white spaces from the  Left  of the string.
+
+//18. includes: returns true or false if the given argument exists or doesn't  exist in the string.
+
+//19. indexOf: returns the index number of given argument in the string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
